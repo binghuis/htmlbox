38aac9759a1603394172aeca63aad5621554de96
宋秉徽 <451000936@qq.com>
2025-02-19 11:55:13 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,9 @@
+# HTML Box
+
+基于 iframe 沙盒实现的 HTML 预览方案。
+
+- console 拦截
+- CSP 内容安全策略处理
+- 新开标签页行为上报
+
+使用时将此项目部署到主应用同源 CDN 上，并基于 postMessage 传输数据。
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <iframe srcDoc=""></iframe>
+    </>
+  );
+}
+
+export default App;
